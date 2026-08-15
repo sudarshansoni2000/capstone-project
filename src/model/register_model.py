e6df4cb774133018c537d4ec9efe98a496fe2086
sudarshansoni2000 <sonisudarshan2017@gmail.com>
2026-08-15 13:56:42 +0530

    CICD run -1

---
+++ src/model/register_model.py
@@ -0,0 +1,112 @@
+import json
+import mlflow
+import logging
+from src.logger import logging
+import os
+import dagshub
+
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+warnings.filterwarnings("ignore")
+
+
+mlflow.set_tracking_uri(
+    "https://dagshub.com/sudarshansoni2000/capstone-project.mlflow"
+)
+dagshub.init(repo_owner='sudarshansoni2000', repo_name='capstone-project', mlflow=True)
+
+
+def load_model_info(file_path: str) -> dict:
+    """Load the model info from a JSON file."""
+    try:
+        with open(file_path, 'r') as file:
+            model_info = json.load(file)
+        logging.debug('Model info loaded from %s', file_path)
+        return model_info
+    except FileNotFoundError:
+        logging.error('File not found: %s', file_path)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error occurred while loading the model info: %s', e)
+        raise
+def register_model(model_name: str, model_info: dict):
+    try:
+        model_uri = model_info["model_uri"]
+
+        logging.info(
+            "Registering model from: %s",
+            model_uri
+        )
+
+        model_version = mlflow.register_model(
+            model_uri=model_uri,
+            name=model_name
+        )
+
+        logging.info(
+            "Model %s version %s registered successfully",
+            model_name,
+            model_version.version
+        )
+
+    except Exception as e:
+        logging.exception(
+            "Error during model registration: %s",
+            e
+        )
+        raise
+
+# def register_model(model_name: str, model_info: dict):
+#     """Register model in MLflow Model Registry."""
+
+#     try:
+
+#         run_id = model_info["run_id"]
+#         model_path = model_info["model_path"]
+
+#         model_uri = f"runs:/{run_id}/{model_path}"
+
+#         logging.info("Registering model from: %s", model_uri)
+
+#         client = mlflow.tracking.MlflowClient()
+
+#         artifacts = client.list_artifacts(run_id)
+
+#         logging.info(
+#             "Artifacts found: %s",
+#             [artifact.path for artifact in artifacts]
+#         )
+
+#         model_version = mlflow.register_model(
+#             model_uri=model_uri,
+#             name=model_name
+#         )
+
+#         logging.info(
+#             "Model %s version %s registered successfully",
+#             model_name,
+#             model_version.version
+#         )
+
+#     except Exception as e:
+
+#         logging.exception(
+#             "Error during model registration: %s",
+#             e
+#         )
+
+#         raise
+
+def main():
+    try:
+        model_info_path = 'reports/experiment_info.json'
+        model_info = load_model_info(model_info_path)
+        
+        model_name = "my_model"
+        register_model(model_name, model_info)
+    except Exception as e:
+        logging.error('Failed to complete the model registration process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
