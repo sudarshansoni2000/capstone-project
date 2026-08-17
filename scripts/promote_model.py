fd5c34cd465325562830e561ed798996573e2cf2
sudarshansoni2000 <sonisudarshan2017@gmail.com>
2026-08-17 20:27:22 +0530

    CI CD 12

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -10,28 +10,28 @@ def promote_model():
     # 1. Set up DagsHub credentials
     # ---------------------------------------------------------
 
-    dagshub_token = os.getenv("CAPSTONE_TEST")
+    # dagshub_token = os.getenv("CAPSTONE_TEST")
 
-    if not dagshub_token:
-        raise EnvironmentError(
-            "CAPSTONE_TEST environment variable is not set"
-        )
+    # if not dagshub_token:
+    #     raise EnvironmentError(
+    #         "CAPSTONE_TEST environment variable is not set"
+    #     )
 
-    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+    # os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    # os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-    dagshub_url = "https://dagshub.com"
-    repo_owner = "sudarshansoni2000"
-    repo_name = "capstone-project"
+    # dagshub_url = "https://dagshub.com"
+    # repo_owner = "sudarshansoni2000"
+    # repo_name = "capstone-project"
 
     # ---------------------------------------------------------
     # 2. Set MLflow tracking URI
     # ---------------------------------------------------------
 
-    # mlflow.set_tracking_uri(
-    #     "https://dagshub.com/sudarshansoni2000/capstone-project.mlflow"
-    # )
-    # dagshub.init(repo_owner='sudarshansoni2000', repo_name='capstone-project', mlflow=True)
+    mlflow.set_tracking_uri(
+        "https://dagshub.com/sudarshansoni2000/capstone-project.mlflow"
+    )
+    dagshub.init(repo_owner='sudarshansoni2000', repo_name='capstone-project', mlflow=True)
 
     # mlflow.set_tracking_uri(
     #     f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow"
